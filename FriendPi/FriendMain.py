04d21283d37664086545a3eafd7046a6bdd357a1
mads118 <vpmads@gmail.com>
2018-12-10 10:01:03 +0100

    nogetnyt

--- FriendPi/FriendMain.py
+++ FriendPi/FriendMain.py
@@ -2,22 +2,13 @@ import socket
 import time
 from sense_hat import SenseHat
 
-friendsID = ""
-friendName = "data"
-gender = ""
-thirst = ""
-hunger =" "
-task = ""
-fun = ""
-dress = ""
-
 def getDataBroadcast ():
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     client.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     client.bind(("", 4000))
     while True:
-        data  = client.recvfrom(1024) 
-      
+        data, addr = client.recvfrom(1024)
+
     return data
 
 def mainmenu ():
